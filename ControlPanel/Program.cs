59ac20fb158d45bd6df46b9d294ec8e9a36d835b
bondarev <bondarev@promit-ek.ru>
2025-12-05 11:39:44 +0500

    Пересоздал ui

--- ControlPanel/Program.cs
+++ ControlPanel/Program.cs
@@ -1,11 +1,11 @@
-using Promtech_Hackaton.Components;
+using ControlPanel.Components;
 using MudBlazor.Services;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
 builder.Services.AddMudServices();
 
 var app = builder.Build();
---
+++ UserApi/Controllers/AuthController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UserApi.Data;
+using UserApi.Entities;
+
+namespace UserApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AuthController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] User loginData)
+        {
+            var user = await _context.Users
+                .FirstOrDefaultAsync(u => u.Username == loginData.Username && u.Password == loginData.Password);
+
+            if (user == null)
+                return Unauthorized(new { message = "Неверный логин или пароль" });
+
+            // Пока просто возвращаем роль, токен можно добавить потом
+            return Ok(new { user.Username, user.Role });
+        }
+
+        [HttpGet("users")]
+        public async Task<IActionResult> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+            return Ok(users);
+        }
+    }
+}
